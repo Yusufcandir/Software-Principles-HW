8acda88beb801e95af6a2abbac10fe57d7ce8f6b
Yusuf Candır <yusufcandir30@gmail.com>
2024-01-11 17:37:58 +0300

    Satisfying requirement one

---
+++ hmk1/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("The numbers between " + num1 + " and " + num2 + " are:");
+        for (int i = num1 + 1; i < num2; i++) {
+            System.out.println(i);
+        }
+    }
+}
